314b0f4c4856685608dd47dfa13d0972ad206f5d
oterl <lucasotermin@gmail.com>
2023-02-23 18:07:32 -0300

    django5 jinja

--- miapp/models.py
+++ miapp/models.py
@@ -12,6 +12,7 @@ class Tarea(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    hecho = models.BooleanField(default=False) # De esta manera coloco un booleano para saber si la tarea fue hecha o no.
 
     def __str__(self):
         return self.titulo
